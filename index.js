c0d76acc5e7503e5ebf26dc1749156c34b7a0bd3
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-07-06 17:06:23 +0530

    5

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ import { createSpinner } from 'nanospinner';
 console.log(chalk.bgGreen('HElloWorld!'));
 
 let playerName ;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow('Welcome to CLI Game');
+    await sleep();
+    rainbowTitle.stop();
+    console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')}
+    1. You will be asked a series of questions.
+    2. Answer them correctly to win the game.
+    3. Have fun!
+    `);
+}
