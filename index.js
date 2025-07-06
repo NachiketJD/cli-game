bf92f1818424f9612c65de3979948c9c33994095
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-07-06 17:04:17 +0530

    3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from "inquirer";
+import gradient from 'gradient-string';
+import figlet from 'figlet';
+import chalkAnimation from 'chalk-animation';
+import { createSpinner } from 'nanospinner';
+
+console.log(chalk.bgGreen('HElloWorld!'));
